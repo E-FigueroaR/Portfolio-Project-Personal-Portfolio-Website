bd6a7bf18d2661ba26424745fe66bea0e87e6998
E-FigueroaR <86622640+E-FigueroaR@users.noreply.github.com>
2021-07-13 17:59:40 -0500

    Add files via upload

---
+++ Personal-Portfolio-Website/interct.js
@@ -0,0 +1,12 @@
+
+
+let show = document.querySelector('a');
+
+let showthis = document.getElementsByClassName('hide');
+
+
+document.querySelector('.hide').addEventListener('click', function(event){
+  event.target.style.display = "visible";
+})
+
+
